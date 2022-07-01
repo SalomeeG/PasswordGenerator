17c52f2435ee0ea05e2ee2055afcf9e911093eaf
SalomeeG <Salome_Gogishvili@epam.com>
2022-07-01 15:46:35 +0400

    initial commit

---
+++ PasswordGenerator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace PasswordGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] salt = new byte[16];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(salt);
+            }
+
+            Console.WriteLine($"Hashed Password: {GeneratePasswordHashUsingSalt("pass", salt)}");
+        }
+
+        public static string GeneratePasswordHashUsingSalt(string passwordText, byte[] salt)
+        {
+            var iterate = 10000;
+            var length = 36;
+
+            var hash = Rfc2898DeriveBytes.Pbkdf2(passwordText, salt, iterate, HashAlgorithmName.SHA1, length);
+
+            return Convert.ToBase64String(hash);
+        }
+    }
+}
